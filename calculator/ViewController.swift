8c62acc61ca0e371359b771877e7d72a9bce64c3
ahmed <ahmedhmedo73@gmail.com>
2020-06-21 05:22:03 -0700

    finished ui design

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -29,19 +29,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var modulus: UIButton!
     @IBOutlet weak var sign: UIButton!
     @IBOutlet weak var Ac: UIButton!
+    @IBOutlet weak var screen: UILabel!
     
+    var Number: Int = 0
+    var count: Int = 0
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        func setOperationUi(button: UIButton){
-            button.backgroundColor = UIColor.orange
-            button.
-        }
-        setOperationUi(button: equal)
+        
         
     }
-
-
+    func appendDigit (digit: Int){
+        if count < 5 {
+        count += 1
+        Number = Number  * 10 + digit
+        screen.text = String(Number)
+        }
+    }
+    @IBAction func zeroClicked(_ sender: UIButton) {
+        Number = Number  * 10
+        screen.text = String(Number)
+    }
+    @IBAction func oneClicked(_ sender: UIButton) {
+        appendDigit(digit: 1)
+    }
+    @IBAction func twoClicked(_ sender: UIButton) {
+        appendDigit(digit: 2)
+    }
+    @IBAction func threeClicked(_ sender: UIButton) {
+        appendDigit(digit: 3)
+    }
+    @IBAction func fourClicked(_ sender: UIButton) {
+        appendDigit(digit: 4)
+    }
+    @IBAction func fiveClicked(_ sender: UIButton) {
+        appendDigit(digit: 5)
+    }
+    @IBAction func sixClicked(_ sender: UIButton) {
+        appendDigit(digit: 6)
+    }
+    @IBAction func sevenClicked(_ sender: UIButton) {
+        appendDigit(digit: 7)
+    }
+    @IBAction func eightClicked(_ sender: UIButton) {
+        appendDigit(digit: 8)
+    }
+    @IBAction func nineClicked(_ sender: UIButton) {
+        appendDigit(digit: 9)
+    }
+    
 }
 
