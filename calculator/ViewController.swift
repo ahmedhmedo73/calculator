26f8ad40242bcc66f305cec8a84b3eca648bae23
ahmed <ahmedhmedo73@gmail.com>
2020-06-21 01:15:55 -0700

    First commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    @IBOutlet weak var four: UIButton!
+    @IBOutlet weak var five: UIButton!
+    @IBOutlet weak var six: UIButton!
+    @IBOutlet weak var seven: UIButton!
+    @IBOutlet weak var eight: UIButton!
+    @IBOutlet weak var nine: UIButton!
+    @IBOutlet weak var deceimal: UIButton!
+    @IBOutlet weak var equal: UIButton!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    @IBOutlet weak var multiply: UIButton!
+    @IBOutlet weak var divide: UIButton!
+    @IBOutlet weak var modulus: UIButton!
+    @IBOutlet weak var sign: UIButton!
+    @IBOutlet weak var Ac: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        func setOperationUi(button: UIButton){
+            button.backgroundColor = UIColor.orange
+            button.
+        }
+        setOperationUi(button: equal)
+        
     }
 
 
