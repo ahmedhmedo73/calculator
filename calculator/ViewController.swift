76b78d8a4cd736aa61085a893bb03cd56588385b
ahmed <ahmedhmedo73@gmail.com>
2020-06-21 17:43:10 -0700

    added actions for plus, minus, divide, multiply, modulus ,Ac and equal and their functions

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -31,25 +31,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var Ac: UIButton!
     @IBOutlet weak var screen: UILabel!
     
-    var Number: Int = 0
-    var count: Int = 0
+    var Raduis: Int = 40
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        deceimal.layer.cornerRadius = CGFloat(Raduis)
+        equal.layer.cornerRadius = CGFloat(Raduis)
+        one.layer.cornerRadius = CGFloat(Raduis)
+        two.layer.cornerRadius = CGFloat(Raduis)
+        three.layer.cornerRadius = CGFloat(Raduis)
+        plus.layer.cornerRadius = CGFloat(Raduis)
+        four.layer.cornerRadius = CGFloat(Raduis)
+        five.layer.cornerRadius = CGFloat(Raduis)
+        six.layer.cornerRadius = CGFloat(Raduis)
+        seven.layer.cornerRadius = CGFloat(Raduis)
+        eight.layer.cornerRadius = CGFloat(Raduis)
+        nine.layer.cornerRadius = CGFloat(Raduis)
+        minus.layer.cornerRadius = CGFloat(Raduis)
+        divide.layer.cornerRadius = CGFloat(Raduis)
+        multiply.layer.cornerRadius = CGFloat(Raduis)
+        modulus.layer.cornerRadius = CGFloat(Raduis)
+        sign.layer.cornerRadius = CGFloat(Raduis)
+        Ac.layer.cornerRadius = CGFloat(Raduis)
+        zero.layer.cornerRadius = 30
         
     }
+    var input: Int = 0
+    var firstNumber: Int = 0
+    var secondNumber: Int = 0
+    var result: Int = 0
+    var count: Int = 0
+    var operation: Character = " "
+    var firstOrSecond: Bool = true
+    
     func appendDigit (digit: Int){
         if count < 5 {
-        count += 1
-        Number = Number  * 10 + digit
-        screen.text = String(Number)
+        input = input  * 10 + digit
+        if input != 0 {
+           count += 1
+        }
+        if firstOrSecond {
+              firstNumber = input
+              screen.text = String(firstNumber)
+        }else{
+              secondNumber = input
+              screen.text = String(firstNumber) + " " + String(operation) + " " + String(secondNumber)
         }
     }
+    }
     @IBAction func zeroClicked(_ sender: UIButton) {
-        Number = Number  * 10
-        screen.text = String(Number)
+        appendDigit(digit: 0)
     }
     @IBAction func oneClicked(_ sender: UIButton) {
         appendDigit(digit: 1)
@@ -77,6 +110,74 @@ class ViewController: UIViewController {
     }
     @IBAction func nineClicked(_ sender: UIButton) {
         appendDigit(digit: 9)
+    }
+    @IBAction func plusClicked(_ sender: UIButton) {
+        operation = "+"
+        screen.text = String(input) + " + "
+        input = 0
+        firstOrSecond = false
+    }
+    @IBAction func minusClicked(_ sender: UIButton) {
+        operation = "-"
+        screen.text = String(input) + " - "
+        input = 0
+        firstOrSecond = false
+    }
+    @IBAction func multiplyClicked(_ sender: UIButton) {
+        operation = "*"
+        screen.text = String(input) + " * "
+        input = 0
+        firstOrSecond = false
+    }
+    @IBAction func divideClicked(_ sender: UIButton) {
+        operation = "/"
+        screen.text = String(input) + " / "
+        input = 0
+        firstOrSecond = false
+    }
+    
+    
+    @IBAction func modulus(_ sender: UIButton) {
+        operation = "%"
+        screen.text = String(input) + " % "
+        input = 0
+        firstOrSecond = false
+    }
+    
+    
+    @IBAction func equalClicked(_ sender: UIButton) {
+        switch operation {
+        case "+" :
+            result = firstNumber + secondNumber
+        case "-" :
+            result = firstNumber - secondNumber
+        case "/" :
+            result = firstNumber / secondNumber
+        case "*" :
+            result = firstNumber * secondNumber
+        case "%" :
+            result = firstNumber % secondNumber
+        default:
+            screen.text = ""
+        }
+        screen.text = String(firstNumber) + " " + String(operation) + " " + String(secondNumber) + " = "+String(result)
+        count = 0
+        firstOrSecond = true
+        input = 0
+        firstNumber = 0
+        secondNumber = 0
+    }
+    
+    
+    @IBAction func acClicked(_ sender: UIButton) {
+         screen.text = ""
+         count = 0
+         firstOrSecond = true
+         input = 0
+         firstNumber = 0
+         secondNumber = 0
+         operation = " "
+         result = 0
     }
     
 }
